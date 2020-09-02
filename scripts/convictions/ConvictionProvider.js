6fd6e6dbf2c0f9f43973934495d5ad8c0258390a
Terra Roush <terra.roush@gmail.com>
2020-09-02 08:20:15 -0500

    used .sort() method to alphabetize my dropdown of crimes

--- scripts/convictions/ConvictionProvider.js
+++ scripts/convictions/ConvictionProvider.js
@@ -8,7 +8,7 @@ export const getConvictions = () => {
   return fetch("https://criminals.glassdale.us/crimes")
     .then((response) => response.json())
     .then((parsedConvictions) => {
-      console.table(parsedConvictions);
+    //   console.table(parsedConvictions);
       convictions = parsedConvictions;
     });
 };
