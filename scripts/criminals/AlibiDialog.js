aabc4fb86a21892aa4778221781e83d3e475c6fc
Terra Roush <terra.roush@gmail.com>
2020-09-08 11:36:20 -0500

    created AlibiDialog.js connected it to the event hub, set up a couple event listeners to render inner html when the alibi button is clicked. beta stage, needs adjustments to get the data to dissappear when clicked twice

---
+++ scripts/criminals/AlibiDialog.js
@@ -0,0 +1,35 @@
+import { useCriminals } from "./CriminalProvider.js";
+
+const eventHub = document.querySelector(".container");
+
+
+eventHub.addEventListener("associatesClicked", (event) => {
+  // display all associates for criminal
+  const targetCriminal = useCriminals().find((criminal) => {
+    return criminal.id === parseInt(event.detail.chosenCriminal);
+  });
+
+  const alibiTarget = document.querySelector(
+    `.alibiDialog--${targetCriminal.id}`
+  );
+  const hTarget = document.querySelector("h4");
+  
+  if (alibiTarget.contains(hTarget)) {
+      alibiTarget.innerHTML = "";
+    } else {
+        alibiTarget.innerHTML = `${targetCriminal.known_associates
+            .map((associate) => {
+                return `
+                <h4>${associate.name}</h4>
+                <div>${associate.alibi}</div>
+            `;
+      })
+      .join("")}`;
+  }
+});
+
+export const AlibiDialog = (id) => {
+  return `
+    <span class="alibiDialog--${id}"></span>
+    `;
+};
--- scripts/notes/NoteForm.js
+++ scripts/notes/NoteForm.js
@@ -6,7 +6,7 @@ const contentTarget = document.querySelector(".noteFormContainer");
 
 //  This is an event listener that defines some variables and invokes a function: Listen for a click when the click is on the save button. variables to watch for are two spots on the dom referring to the textarea input and the criminal dropdown select. Watch for the value of the criminal dropdown to be anything other than 0. Then it defines the variable newNote as an object with three key/value pairs. These coordinate with our JSON note objects. When anything other than 0 is selected, the values of the object's properties change accordingly.Those values are literally the new note and those values are stored in the variable newNote. Now we invoke saveNote with our newNote. the saveNote function posts our note to our API as JSON string! Then our app gets all the notes back from the API, and then it tells something that there is a state change!! 
 eventHub.addEventListener("click", (clickEvent) => {
-  if (clickEvent.target.id === "saveNote") {
+  if (clickEvent.target.id === "saveNote-btn") {
     const noteContent = document.querySelector("#note-text");
     const noteCriminal = document.querySelector("#note-criminalSelect");
 
@@ -37,7 +37,7 @@ const render = (criminalArray) => {
           }
     </select>
 
-    <button type="button" id="saveNote">Save Note</button> 
+    <button type="button" id="saveNote-btn">Save Note</button> 
     
     `;
 };
